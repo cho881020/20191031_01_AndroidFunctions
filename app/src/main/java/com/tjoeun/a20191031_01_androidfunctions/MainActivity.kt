3a980399fc43ac55d10a95158602a24f5901a35e
kj_cho <kj_cho@idealidea.co.kr>
2019-10-31 22:08:49 +0900

    첨부된 데이터 받아서 활용하기. (MainActivity)

--- app/src/main/java/com/tjoeun/a20191031_01_androidfunctions/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191031_01_androidfunctions/MainActivity.kt
@@ -4,12 +4,21 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        intent에 첨부된 데이터를 찾아주는 코드.
+        var receivedName = intent.getStringExtra("name")
+        var receivedNumber = intent.getStringExtra("memberNumber")
+
+        memberNumTxt.text = "${receivedNumber}번 회원"
+        memberNameTxt.text = receivedName
+
 
     }
 }
