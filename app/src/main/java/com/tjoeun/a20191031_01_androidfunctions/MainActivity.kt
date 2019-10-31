99cf0a929c6645541dc44f5c02e636b1ce8d4f18
kj_cho <kj_cho@idealidea.co.kr>
2019-10-31 20:39:49 +0900

    매니페스트 변경 실습  - 앱 아이콘 변경  - 앱 이름 변경  - 테마 색상  - 테마 타이틀바

--- app/src/main/java/com/tjoeun/a20191031_01_androidfunctions/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191031_01_androidfunctions/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.tjoeun.a20191031_01_androidfunctions
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
